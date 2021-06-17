6f7aa55773b316611897ed697042ec7cf4401694
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-16 19:35:59 -0500

    added CRUD for USER model

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,4 +13,145 @@ router.get("/", (req, res) => {
     });
 });
 
+// get a user by ID
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "content", "created_at", "updated_at"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at", "updated_at"],
+        include: {
+          model: Post,
+          attributes: ["id", "title"],
+        },
+      },
+    ],
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// create a new user
+router.post("/", (req, res) => {
+  // expects {username: 'electro', password: 'spark'}
+  User.create({
+    username: req.body.username,
+    password: req.body.password,
+  })
+    .then((dbData) => {
+      req.session.save(() => {
+        req.session.user_id = dbData.id;
+        req.session.username = dbData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// LOGIN a user
+router.post("/login", (req, res) => {
+  // expects {username: 'electro', password: 'spark'}
+  User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((dbData) => {
+    if (!dbData) {
+      res.status(400).json({ message: "No user with that username!" });
+      return;
+    }
+
+    const validPassword = dbData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbData.id;
+      req.session.username = dbData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbData, message: "You are now logged in!" });
+    });
+  });
+});
+
+// logout a USER
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+// UPDATE a user
+router.put("/:id", (req, res) => {
+  // expects {username: 'electro', password: 'spark'}
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a user
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbData) => {
+      if (!dbData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
